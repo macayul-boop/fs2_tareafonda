6ac88aceba69d3ee31986b06c8b48de60cca966e
Matias Cayul <ma.cayul@duocuc.cl>
2026-09-15 20:49:46 -0300

    Creacion de los archivos repository para las entidades bebida y venta

---
+++ backend/src/main/java/cl/dsy1104/fonda/repository/BebidaRepository.java
@@ -0,0 +1,7 @@
+package cl.dsy1104.fonda.repository;
+
+import cl.dsy1104.fonda.model.Bebida;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BebidaRepository extends JpaRepository<Bebida, Long> {
+}
---
+++ backend/src/main/java/cl/dsy1104/fonda/repository/VentaRepository.java
@@ -0,0 +1,7 @@
+package cl.dsy1104.fonda.repository;
+
+import cl.dsy1104.fonda.model.Venta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VentaRepository extends JpaRepository<Venta, Long> {
+}
